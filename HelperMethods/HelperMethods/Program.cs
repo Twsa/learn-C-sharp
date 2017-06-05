bd9be62e835db04fcc9e2f2f3df4a3912635bd62
刘传佳 <liuchuanjiacl@gmail.com>
2017-06-05 16:36:44 +0800

    创建和调用简单的Helper方法
        方法签名 ；方法重载，不同的类型的参数回寻找不同的的对应的方法
    void means doesn't return any value to you

---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue = superSecretFormula("Twsa Liu");
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+
+        private static string superSecretFormula()
+        {
+            //  some cool stuff here
+            return "Hello World!";
+        }
+
+        private static string superSecretFormula(string name)
+        {
+            return String.Format("Hello,{0}", name);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,7 +10,10 @@ IDE:virtual studio professional 2017
 
 2017/06/05
 1、添加 Decisions
-note:if语句分支决策，Console.WriteLine ,内部字符串重载
+note:if语句分支决策，Console.WriteLine ,内部字符串重载，变量输入
 2、运算符，表达式和语句
-3、迭代
+3、迭代，for语句迭代
 4、数组
+5、创建和调用简单的Helper方法  
+    方法签名 ；方法重载，不同的类型的参数回寻找不同的的对应的方法
+void means doesn't return any value to you
