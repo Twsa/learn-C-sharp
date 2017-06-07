1ca59a4be9bec7ca2e17de090e26732132f922fb
刘传佳 <liuchuanjiacl@gmail.com>
2017-06-07 12:14:39 +0800

    了解命名空间和添加程序集引用
    使用 Tabor.dll文件并理解命名空间，获取html数据

---
+++ NamespacesAndRefencingAssmbling/NamespacesAndRefencingAssmbling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabor;
+
+namespace NamespacesAndRefencingAssmbling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StreamReader myStreamReader = new StreamReader();
+            Bob bob = new Bob();
+
+            string html = bob.Lookup("https://github.com/Twsa");
+
+            Console.WriteLine(html);
+            Console.ReadLine();
+        }
+    }
+}
